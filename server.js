f4b321d7d12527d5b89e9e3bcfc147117e884257
kartikeya0169 <kartikeyemittal@gmail.com>
2018-07-27 19:25:33 +0530

    setup heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const hbs=require('hbs');
+const port=process.env.PORT ||3000;
 var app=express();
 app.set('view engine','hbs');
 app.use(express.static( './public'));
 app.get('/',(req,res)=>{
-  res.download('C:/Users/HP/Documents/yt.pdf');
+  res.send('<h1>hello express</h1>')
+
 });
 app.get('/about',(req,res)=>{
   res.render('about.hbs',{
@@ -13,4 +15,6 @@ app.get('/about',(req,res)=>{
   });
 })
 
-app.listen(3000);
+app.listen(port,()=>{
+  console.log("server is up on"+port);
+});
